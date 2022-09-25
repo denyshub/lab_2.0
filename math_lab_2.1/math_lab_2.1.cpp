18ecc33f6bd1a9d11b0b18b55c4256467b60638b
Your Name <you@example.com>
2022-09-25 19:39:27 +0300

    first master commit

---
+++ math_lab_2.1/math_lab_2.1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	double a;
+	double z1;
+	//double z2;
+	cout << "Enter an number: ";  cin >> a;
+	z1 = (1.0 - 1.0 / 4 * pow(2 * sin(a) * cos(a), 2) + (cos(2*a))); 
+	cout <<"Result, z1: " << z1 << endl;
+	//z2 = pow(cos(a), 2) + pow(cos(a), 4);
+	//cout << "Result, z2: " << z2 << endl;
+	//if (z1 == z2) {
+		//cout << "That`s right!";
+//	}
+	  
+
+
+
+
+
+	return 0;
+}
